037209fc309ff420b413da081293c38836954331
ambsenyestudi <adrifre7@hotmail.com>
2020-01-11 09:26:31 +0100

    Documenting methods

--- src/FluentValidationApi/FluentValidationApi/Controllers/TVGuideController.cs
+++ src/FluentValidationApi/FluentValidationApi/Controllers/TVGuideController.cs
@@ -7,16 +7,23 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace FluentValidationApi.Controllers
 {
+    /// <summary>
+    /// Manages de TV Guide
+    /// </summary>
     [Route("api/[controller]")]
     [ApiController]
     public class TVGuideController : ControllerBase
     {
+        /// <summary>
+        /// Gets you the shows from the tv guide
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
         public Task<IEnumerable<string>> Get()
         {
             return Task<IEnumerable<string>>.Factory
-                .StartNew(() => new List<string> 
-                { 
+                .StartNew(() => new List<string>
+                {
                     "sports", "news", "weatherforecast"
                 });
         }
