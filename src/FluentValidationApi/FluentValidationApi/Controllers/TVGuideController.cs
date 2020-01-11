8cf975c0f027a18738288b3232fc0e713f644b06
ambsenyestudi <adrifre7@hotmail.com>
2020-01-11 09:10:39 +0100

    api created

---
+++ src/FluentValidationApi/FluentValidationApi/Controllers/TVGuideController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FluentValidationApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TVGuideController : ControllerBase
+    {
+        [HttpGet]
+        public Task<IEnumerable<string>> Get()
+        {
+            return Task<IEnumerable<string>>.Factory
+                .StartNew(() => new List<string> 
+                { 
+                    "sports", "news", "weatherforecast"
+                });
+        }
+    }
+}
